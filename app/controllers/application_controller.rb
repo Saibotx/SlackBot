8d676df8f74ddbfb0860c8ec2c2c97baf442c2e0
tobias Gu <tobias.goo@gmail.com>
2019-08-19 16:07:06 +0200

    removed more useless links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,22 +8,22 @@ class ApplicationController < ActionController::Base
   end
 
   def create
+    #all POST incoming requests must be verified
     unless request_verified?
       puts "unauthorized"
       json_response :status => :unauthorized
     end
-    #if setting up endpoint
+    #used for setting up endpoint from slack dashboard.
     if params["challenge"]
       challenge = { :challenge => params["challenge"]}
       json_response challenge
     end
 
-    #handle user_update event
+    #distinguish between types of webhooks/events
     event_type = params.dig("event", "type")
     case event_type
     when "user_change"
       user_params = params.dig("event","user")
-      puts "userchange. New user is #{user_params}"
       create_or_update_user_service = SlackUserService.new(user_params)
       create_or_update_user_service.perform
     else
@@ -38,11 +38,9 @@ class ApplicationController < ActionController::Base
     timestamp = request.headers["X-Slack-Request-Timestamp"]
     signature = request.headers["X-Slack-Signature"]
     signing_secret = ENV.fetch("SLACK_SIGNING_SECRET")
-
     if Time.at(timestamp.to_i) < 5.minutes.ago
       return false # expired
     end
-
     basestring = "v0:#{timestamp}:#{request.body.read}"
     my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
 
